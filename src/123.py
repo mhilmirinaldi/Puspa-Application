7c6ff077673feed0470bc66eb7d37b50ee050c7b
zaydzuhri <zaydzuhri@gmail.com>
2022-04-15 21:50:15 +0700

    f*ck pytest, deal with it later

--- src/123.py
+++ src/123.py
@@ -1,3 +1,4 @@
+import pytest
 # Print hello world 10 times
 for i in range(10):
     print("Hello World")
@@ -6,7 +7,3 @@ A = 1
 B = 2
 C = 3
 print(A + B + C)
-
-assert A == 1
-assert B == 2
-assert C == 3
